c3121cc9965021326323c9ba4ed73165f7cb2a0a
Ben Roberts <ben@thatmustbe.me>
2017-06-28 21:42:55 -0400

    fix rendering a bit

--- index.php
+++ index.php
@@ -1,49 +1,51 @@
+<!DOCTYPE html>
 <html>
-<head>
+  <head>
+    <title>Validate JF2</title>
 <style>
 form{width:700px; margin-left:auto; margin-right:auto; position:relative; text-align:center;}
 .button{width:100%;}
 textarea{width:100%; min-height:200px;}
 .results{width:700px;margin-left:auto; margin-right:auto;}
 </style>
-</head>
-<body>
+  </head>
+  <body>
 
-<form method="POST">
+    <form method="POST">
 
-<textarea name="data" placeholder="jf2 data here"><?php echo $_POST['data']?></textarea>
-<br>
-<label for="fix_quotes">Auto Convert all single quotes to double quotes: </label><input id="fix_quotes" type="checkbox" name="fix_quotes" value="true"  <?php echo (isset($_POST['fix_quotes']) ? 'checked="checked"' : '') ?>/><br>
-<input class="button" type="submit" name="submit" value="Validate"/>
+      <textarea name="data" placeholder="jf2 data here"><?php echo $_POST['data']?></textarea>
+      <br>
+      <label for="fix_quotes">Auto Convert all single quotes to double quotes: </label><input id="fix_quotes" type="checkbox" name="fix_quotes" value="true"  <?php echo (isset($_POST['fix_quotes']) ? 'checked="checked"' : '') ?>/><br>
+      <input class="button" type="submit" name="submit" value="Validate"/>
 
-</form>
+    </form>
 
-<div class="results">
-<?php
-if(isset($_POST['data'])){
+    <div class="results">
+    <?php
+    if(isset($_POST['data'])){
 
-    require_once __DIR__ . '/validator.php';
+        require_once __DIR__ . '/validator.php';
 
-    $input = $_POST['data'];
+        $input = $_POST['data'];
 
-    $fix_quotes = isset($_POST['fix_quotes']);
+        $fix_quotes = isset($_POST['fix_quotes']);
 
-    $results = do_validate($input, $fix_quotes);
+        $results = do_validate($input, $fix_quotes);
 
-    $success = true;
-    foreach($results as $result){
-        $type = 'warn';
-        if($result->type == P_ERROR){
-            $success = false;
-            $type = 'error';
+        $success = true;
+        foreach($results as $result){
+            $type = 'warn';
+            if($result->type == P_ERROR){
+                $success = false;
+                $type = 'error';
+            }
+            echo '<div class="result '.$type.'">';
+            echo $result->message ;
+            echo '</div>';
         }
-        echo '<div class="result '.$type.'>';
-        echo $result->message ;
-        echo '</div>';
     }
-}
-?>
-</div>
+    ?>
+    </div>
 
 
 </body>
