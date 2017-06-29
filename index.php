065006e224e70d8f555195923aa618f743309996
Ben Roberts <ben@thatmustbe.me>
2017-06-29 09:24:20 -0400

    much nicer style, fix recursion

--- index.php
+++ index.php
@@ -2,31 +2,24 @@
 <html>
   <head>
     <title>Validate JF2</title>
-<style>
-form{width:700px; margin-left:auto; margin-right:auto; position:relative; text-align:center;}
-.button{width:100%;}
-textarea{width:100%; min-height:200px;}
-.results{width:700px;margin-left:auto; margin-right:auto;}
-.result{margin-top:10px; border:1px solid black;}
-.warning{background-color:#ccf;}
-.error{background-color:#fcc;}
-.success{background-color:#cfc;}
-</style>
+	<link href="style.css" rel="stylesheet">
   </head>
   <body>
 
-    <h1>JF2 Validator</h1>
+    <div class="single-column">
+        <h1>JF2 Validator</h1>
 
-    <form method="POST">
+    <section class="content">
+        <form method="POST">
 
-      <textarea name="data" placeholder="jf2 data here"><?php echo $_POST['data']?></textarea>
-      <br>
-      <label for="fix_quotes">Auto Convert all single quotes to double quotes: </label><input id="fix_quotes" type="checkbox" name="fix_quotes" value="true"  <?php echo (isset($_POST['fix_quotes']) ? 'checked="checked"' : '') ?>/><br>
-      <input class="button" type="submit" name="submit" value="Validate"/>
+          <textarea name="data" placeholder="jf2 data here"><?php echo $_POST['data']?></textarea>
+          <br>
+          <label for="fix_quotes">Auto Convert all single quotes to double quotes: </label><input id="fix_quotes" type="checkbox" name="fix_quotes" value="true"  <?php echo (isset($_POST['fix_quotes']) ? 'checked="checked"' : '') ?>/><br>
+          <input class="button" type="submit" name="submit" value="Validate"/>
 
-    </form>
+        </form>
+    </section>
 
-    <div class="results">
     <?php
     if(isset($_POST['data'])){
 
@@ -36,7 +29,10 @@ textarea{width:100%; min-height:200px;}
 
         $fix_quotes = isset($_POST['fix_quotes']);
 
-        $results = do_validate($input, $fix_quotes);
+        //$results = do_validate($input, $fix_quotes);
+
+        $validator = new JF2Validator();
+        $results = $validator->validate($input, $fix_quotes);
 
         $success = true;
         foreach($results as $result){
@@ -45,14 +41,14 @@ textarea{width:100%; min-height:200px;}
                 $success = false;
                 $type = 'error';
             }
-            echo '<div class="result '.$type.'">';
+            echo '<section class="content result '.$type.'">';
             echo $result->message ;
-            echo '</div>';
+            echo '</section>';
         }
     }
 
     if(isset($success) && $success){
-        echo '<div class="result success">This validator is not yet complete, but theres no errors in it thus far.</div>';
+        echo '<section class=" content result success">This validator is not yet complete, but theres no errors in it thus far.</section>';
     }
 
     ?>
--- validator.php
+++ validator.php
@@ -16,131 +16,199 @@ class ResultMessage
     
 }
 
-function is_hash(array $in)
-{
-    return is_array($in) && count(array_filter(array_keys($in), 'is_string')) > 0;
-}
 
-function do_validate($input, $fix_quotes = false)
+/*
+function do_validate($input, $fix_quotes = false, $jf2feed = false)
 {
-    $results = array();
+    if($jf2feed){
 
-    if($fix_quotes){
-        $input = str_replace("'", '"', $input);
+        return jf2feed_validate($input, $fix_quotes);
+    } else {
+        return jf2_validate($input, $fix_quotes);
     }
-    $parsed = json_decode($input, true);
+}
+function jf2feed_validate($input, $fix_quotes = false)
+{
+}
+ */
 
-	$last_err = json_last_error();
+class JF2Validator
+{
+    public function validate($input, $fix_quotes = false)
+    {
+        $results = array();
 
-	if($last_err){
+        if($fix_quotes){
+            $input = str_replace("'", '"', $input);
+        }
+        $parsed = json_decode($input, true);
 
-		$constants = get_defined_constants(true);
-		$json_errors = array();
-		foreach ($constants["json"] as $name => $value) {
-			if (!strncmp($name, "JSON_ERROR_", 11)) {
-				$json_errors[$value] = $name;
-			}
-		}
+        $last_err = json_last_error();
 
-        $results[] = new ResultMessage(P_ERROR, 'JSON decode error "' . $json_errors[$last_err]. '". Parsing stopped.');
+        if($last_err){
+
+            $constants = get_defined_constants(true);
+            $json_errors = array();
+            foreach ($constants["json"] as $name => $value) {
+                if (!strncmp($name, "JSON_ERROR_", 11)) {
+                    $json_errors[$value] = $name;
+                }
+            }
+
+            $results[] = new ResultMessage(P_ERROR, 'JSON decode error "' . $json_errors[$last_err]. '". Parsing stopped.');
+            return $results;
+        }
+        if(!isset($parsed['type']) && (!isset($parsed['children']) || count($parsed) != 1)){
+            $results[] = new ResultMessage(P_WARN, '"type" field missing. This is not recommended unless it includes only the attribute "children", and nothing else');
+        }
+
+        if(isset($parsed['type']) && (is_array($parsed['type']) || !is_string($parsed['type'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'type field must be a single string', '"type" : ' . print_r($parsed['type'], true));
+        }
+
+        if(isset($parsed['children']) && (!is_array($parsed['children']) || $this->is_hash($parsed['children']))){
+            $results[] = new ResultMessage(P_ERROR, 'children must be serialized as an array []', '"children" : ' . print_r($parsed['children'], true));
+        } elseif (isset($parsed['children'])){
+            $all_are_hashes = true;
+            $error_lines = array();
+            foreach($parsed['children'] as $child){
+                if(!$this->is_hash($child)){
+                    $all_are_hashes = false;
+                    $error_lines[] = $child;
+                }
+            }
+            
+            if(!$all_are_hashes){
+                $results[] = new ResultMessage(P_ERROR, 'children array must contain only objects', '"children" : ' . print_r($error_lines, true));
+            }
+
+        }
+
+        //TODO: top level ONLY
+        if(isset($parsed['references']) && (is_array($parsed['references']) || !is_string($parsed['references'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'references must be serialized as a hash', '"references" : ' . print_r($parsed['references'], true));
+        } 
+        //TODO check possible values
+        if(isset($parsed['lang']) && (is_array($parsed['lang']) || !is_string($parsed['lang'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'lang field must be a single string', '"lang" : ' . print_r($parsed['lang'], true));
+        }
+        //TODO: top level ONLY
+        if(isset($parsed['@context']) && (is_array($parsed['@context']) || !is_string($parsed['@context'] ))){
+            $results[] = new ResultMessage(P_ERROR, '@context field must be a single string', '"@context" : ' . print_r($parsed['@context'], true));
+        }
+        if(isset($parsed['@context']) && trim($parsed['@context']) != 'http://www.w3.org/ns/jf2') {
+            $results[] = new ResultMessage(P_ERROR, '@context field if present, must be "http://www.w3.org/ns/jf2"', '"@context" : ' . print_r($parsed['@context'], true));
+        }
+
+        if(isset($parsed['value']) && (is_array($parsed['value']) || !is_string($parsed['value'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'value field must be a single string', '"value" : ' . print_r($parsed['value'], true));
+        }
+
+        //TODO these doesn't make sense on top level items
+        if(isset($parsed['content-type']) && (is_array($parsed['content-type']) || !is_string($parsed['content-type'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'content-type field must be a single string', '"content-type" : ' . print_r($parsed['content-type'], true));
+        }
+        if(isset($parsed['text']) && (is_array($parsed['text']) || !is_string($parsed['text'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'text field must be a single string', '"text" : ' . print_r($parsed['text'], true));
+        }
+        if(isset($parsed['html']) && (is_array($parsed['html']) || !is_string($parsed['html'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'html field must be a single string', '"html" : ' . print_r($parsed['html'], true));
+        }
+
+
+        $results = array_merge($results, $this->descend_and_recurse($parsed));
+
         return $results;
-	}
-    if(!isset($parsed['type']) && (!isset($parsed['children']) || count($parsed) != 1)){
-        $results[] = new ResultMessage(P_WARN, '"type" field missing. This is not recommended unless it includes only the attribute "children", and nothing else');
+
     }
 
-    if(isset($parsed['type']) && (is_array($parsed['type']) || !is_string($parsed['type'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'type field must be a single string', '"type" : ' . print_r($parsed['type'], true));
+    private function descend_and_recurse($parsed)
+    {
+        $results = array();
+        if($this->is_hash($parsed)){
+            foreach($parsed as $key => $val){
+                if($this->is_hash($val)){
+                    $results = array_merge($results, $this->recursive_validate($val));
+                } elseif(is_array($val)){
+                    $results = array_merge($results, $this->descend_and_recurse($val));
+                }
+            }
+
+        } elseif(is_array($parsed)){
+            foreach($parsed as $val){
+                if($this->is_hash($val)){
+                    $results = array_merge($results, $this->recursive_validate($val));
+                } elseif(is_array($val)){
+                    $results = array_merge($results, $this->descend_and_recurse($val));
+                }
+            }
+        }
+        return $results;
+        //var_dump($parsed);
     }
 
-    if(isset($parsed['children']) && (!is_array($parsed['children']) || is_hash($parsed['children']))){
-        $results[] = new ResultMessage(P_ERROR, 'children must be serialized as an array []', '"children" : ' . print_r($parsed['children'], true));
-    } 
+    private function recursive_validate($parsed)
+    {
+        $results = array();
 
-    //TODO: top level ONLY
-    if(isset($parsed['references']) && (is_array($parsed['references']) || !is_string($parsed['references'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'references must be serialized as a hash', '"references" : ' . print_r($parsed['references'], true));
-    } 
-    //TODO check possible values
-    if(isset($parsed['lang']) && (is_array($parsed['lang']) || !is_string($parsed['lang'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'lang field must be a single string', '"lang" : ' . print_r($parsed['lang'], true));
-    }
-    //TODO: top level ONLY
-    if(isset($parsed['@context']) && (is_array($parsed['@context']) || !is_string($parsed['@context'] ))){
-        $results[] = new ResultMessage(P_ERROR, '@context field must be a single string', '"@context" : ' . print_r($parsed['@context'], true));
-    }
-    if(isset($parsed['@context']) && trim($parsed['@context']) != 'http://www.w3.org/ns/jf2') {
-        $results[] = new ResultMessage(P_ERROR, '@context field if present, must be "http://www.w3.org/ns/jf2"', '"@context" : ' . print_r($parsed['@context'], true));
-    }
-
-    if(isset($parsed['value']) && (is_array($parsed['value']) || !is_string($parsed['value'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'value field must be a single string', '"value" : ' . print_r($parsed['value'], true));
-    }
-
-    //TODO these doesn't make sense on top level items
-    if(isset($parsed['content-type']) && (is_array($parsed['content-type']) || !is_string($parsed['content-type'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'content-type field must be a single string', '"content-type" : ' . print_r($parsed['content-type'], true));
-    }
-    if(isset($parsed['text']) && (is_array($parsed['text']) || !is_string($parsed['text'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'text field must be a single string', '"text" : ' . print_r($parsed['text'], true));
-    }
-    if(isset($parsed['html']) && (is_array($parsed['html']) || !is_string($parsed['html'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'html field must be a single string', '"html" : ' . print_r($parsed['html'], true));
-    }
-
-
-    $results = array_merge($results, descend_and_recurse($parsed));
-
-	return $results;
-
-}
-
-function descend_and_recurse($parsed)
-{
-    $results = array();
-    if(is_hash($parsed)){
-        foreach($parsed as $key => $val){
-            $results = array_merge($results, recursive_validate($val));
+        if(isset($parsed['references']) ){
+            $results[] = new ResultMessage(P_ERROR, 'references is only allowed at the top level', '"references" : ' . print_r($parsed['references'], true));
+        } 
+        if(isset($parsed['@context']) ){
+            $results[] = new ResultMessage(P_ERROR, '@context is only allowed at the top level', '"@context" : ' . print_r($parsed['@context'], true));
         }
 
-    } elseif(is_array($parsed)){
-        foreach($parsed as $val){
-            $results = array_merge($results, descend_and_recurse($val));
+        if(isset($parsed['type']) && (is_array($parsed['type']) || !is_string($parsed['type'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'type field must be a single string', '"type" : ' . print_r($parsed['type'], true));
         }
-    }
-    return $results;
-    //var_dump($parsed);
-}
 
-function recursive_validate($parsed)
-{
-    $results = array();
+        if(isset($parsed['children']) && (!is_array($parsed['children']) || $this->is_hash($parsed['children']))){
+            $results[] = new ResultMessage(P_ERROR, 'children must be serialized as an array []', '"children" : ' . print_r($parsed['children'], true));
+        } elseif (isset($parsed['children'])){
+            $all_are_hashes = true;
+            $error_lines = array();
+            foreach($parsed['children'] as $child){
+                if(!$this->is_hash($child)){
+                    $all_are_hashes = false;
+                    $error_lines[] = $child;
+                }
+            }
+            
+            if(!$all_are_hashes){
+                $results[] = new ResultMessage(P_ERROR, 'children array must contain only objects', '"children" : ' . print_r($error_lines, true));
+            }
 
-    if(isset($parsed['references']) ){
-        $results[] = new ResultMessage(P_ERROR, 'references is only allowed at the top level', '"references" : ' . print_r($parsed['references'], true));
-    } 
-    if(isset($parsed['@context']) ){
-        $results[] = new ResultMessage(P_ERROR, '@context is only allowed at the top level', '"@context" : ' . print_r($parsed['@context'], true));
+        }
+
+        //TODO check possible values
+        if(isset($parsed['lang']) && (is_array($parsed['lang']) || !is_string($parsed['lang'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'lang field must be a single string', '"lang" : ' . print_r($parsed['lang'], true));
+        }
+
+        if(isset($parsed['value']) && (is_array($parsed['value']) || !is_string($parsed['value'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'value field must be a single string', '"value" : ' . print_r($parsed['value'], true));
+        }
+        if(isset($parsed['content-type']) && (is_array($parsed['content-type']) || !is_string($parsed['content-type'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'content-type field must be a single string', '"content-type" : ' . print_r($parsed['content-type'], true));
+        }
+        if(isset($parsed['text']) && (is_array($parsed['text']) || !is_string($parsed['text'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'text field must be a single string', '"text" : ' . print_r($parsed['text'], true));
+        }
+        if(isset($parsed['html']) && (is_array($parsed['html']) || !is_string($parsed['html'] ))){
+            $results[] = new ResultMessage(P_ERROR, 'html field must be a single string', '"html" : ' . print_r($parsed['html'], true));
+        }
+
+        $results = array_merge($results, $this->descend_and_recurse($parsed));
+        
+        return $results;
     }
 
-    //TODO check possible values
-    if(isset($parsed['lang']) && (is_array($parsed['lang']) || !is_string($parsed['lang'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'lang field must be a single string', '"lang" : ' . print_r($parsed['lang'], true));
+    private function is_hash($in)
+    {
+        if(!is_array($in)){
+            return false;
+        }
+        return is_array($in) && count(array_filter(array_keys($in), 'is_string')) > 0;
     }
-
-    if(isset($parsed['value']) && (is_array($parsed['value']) || !is_string($parsed['value'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'value field must be a single string', '"value" : ' . print_r($parsed['value'], true));
-    }
-    if(isset($parsed['content-type']) && (is_array($parsed['content-type']) || !is_string($parsed['content-type'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'content-type field must be a single string', '"content-type" : ' . print_r($parsed['content-type'], true));
-    }
-    if(isset($parsed['text']) && (is_array($parsed['text']) || !is_string($parsed['text'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'text field must be a single string', '"text" : ' . print_r($parsed['text'], true));
-    }
-    if(isset($parsed['html']) && (is_array($parsed['html']) || !is_string($parsed['html'] ))){
-        $results[] = new ResultMessage(P_ERROR, 'html field must be a single string', '"html" : ' . print_r($parsed['html'], true));
-    }
-    
-    return $results;
 }
 
